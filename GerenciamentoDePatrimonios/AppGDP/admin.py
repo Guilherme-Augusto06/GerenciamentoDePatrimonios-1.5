a468303225e5ae9d91901053ed7810feb3e85f65
Guilherme-Augusto06 <ga375464@gmail.com>
2024-10-01 08:01:15 -0300

    feat: migrações das tabelas

--- GerenciamentoDePatrimonios/AppGDP/admin.py
+++ GerenciamentoDePatrimonios/AppGDP/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Senai
 from .models import Inventario
+from .models import Sala
 
 
 admin.site.register(Senai)
 admin.site.register(Inventario)
+admin.site.register(Sala)
